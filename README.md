fe084897ebdecb79f29518f96ca5d066febb9535
Mykhailo Petrenko <mikkicon@gmail.com>
2024-11-24 10:15:02 -0600

    dash map working; fix clustering

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ we had 2 ideas:
 1. sustainability challenge generator bot
 2. volunteering marketplace for local communities
 
-via one good 🏓 match we decided how we can combine them and integrate LLAMA
+via one good 🏓 match we decided how we can combine them - ACID 💊 😜
+
+Austin
+Civic
+Impact
+Driver
+
 
 1. community members submit requests 1.playground swing is broken 2.fence is tilted, 3.racoons unfed
 2. from these requests LLM generates events: 1.fix playground + fence 2.feed racoons
@@ -13,4 +19,6 @@ via one good 🏓 match we decided how we can combine them and integrate LLAMA
 
 LLAMA part
 - we cluster requests by Latitude, Longtitude
+- we calculated cosine similarity for pairs of requests inside cluster
 - We then feed them into LLAMA to output events generated from these requests
+
--- src/lib/cluster_requests.py
+++ src/lib/cluster_requests.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 import os
 
+
 N_CLUSTERS = 3
 
 def cluster_requests():
@@ -11,7 +12,12 @@ def cluster_requests():
   db = client["acip"]
   collection = db["requests"]
 
-  unclustered_requests = list(collection.find({"cluster": {"$exists": False}}))
+  unclustered_requests = list(collection.find({
+      "location": {"$exists": True},
+      "location.lat": {"$ne": None},
+      "location.long": {"$ne": None},
+      "cluster": {"$exists": False}  # Fixed the syntax here
+  }))
 
   if not unclustered_requests:
       print("No unclustered data found.")
@@ -19,10 +25,11 @@ def cluster_requests():
 
   coordinates = np.array([[req["location"]["lat"], req["location"]["long"]] for req in unclustered_requests])
 
+  CLUSTERED_COUNT = 0
   labels = KMeans(n_clusters=N_CLUSTERS, random_state=42).fit(coordinates).predict(coordinates).tolist()
   
   for id, label in enumerate(labels):
-      collection.update_one({"_id": unclustered_requests[id]["_id"]}, {"$set": {"cluster": label}})
+      collection.update_one({"_id": unclustered_requests[id]["_id"]}, {"$set": {"cluster": label+CLUSTERED_COUNT}})
 
   print("Clustering complete!")
 
